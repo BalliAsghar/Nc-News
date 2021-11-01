2bea9410423d2d19b7ca6f91f0ee036d797a86a7
BalliAsghar <ballihacker@outlook.com>
2021-11-01 13:47:11 +0000

    express setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+//  load environment variables
+require("dotenv").config();
+
+// App initialization
+const app = express();
+
+// Index Route
+app.get("/", (req, res) => res.send("Hello"));
+
+// Get Port
+const PORT = process.env.PORT || 3000;
+
+// listen Server
+app.listen(PORT, () => console.log(`App Running on Port ${PORT}`));

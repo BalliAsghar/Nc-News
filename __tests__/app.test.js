d701ff91a3e3fe2b98a16f87913e6d9063d8fcdf
BalliAsghar <ballihacker@outlook.com>
2021-11-01 14:19:57 +0000

    Invalid EndPoint & test ✓

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ const app = require("../app");
 // afterAll(() => db.end());
 
 describe("App", () => {
+  describe("Invalid Endpoint", () => {
+    test("should receive 404 Status And 'Invalid EndPoint' in res.body", () => {
+      return request(app)
+        .get("/SomeEndpoint")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.message).toBe("Invalid EndPoint");
+        });
+    });
+  });
+
   describe("GET /", () => {
     test("Should receive 200 ok Status", () => {
       return request(app).get("/").expect(200);
@@ -17,7 +28,6 @@ describe("App", () => {
         .get("/")
         .expect(200)
         .then(({ body }) => {
-          console.log(body);
           expect(body.message).toBe("Hello");
         });
     });
--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
-
-//  load environment variables
-require("dotenv").config();
-
 // App initialization
 const app = express();
 
 // Index Route
 app.get("/", (req, res) => res.send({ message: "Hello" }));
 
+app.all("/*", (req, res) =>
+  res.status(404).send({ message: "Invalid EndPoint" })
+);
+
 // Get Port
-const PORT = process.env.PORT || 3000;
+const PORT = 9090;
 
 // listen Server
 app.listen(PORT, () => console.log(`App Running on Port ${PORT}`));

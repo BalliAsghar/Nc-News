8ca88f77b4044866e8292edd509742625bf3b047
BalliAsghar <ballihacker@outlook.com>
2021-11-02 14:56:05 +0000

    GET - /api/articles - refactored

--- models/articles.model.js
+++ models/articles.model.js
@@ -5,7 +5,29 @@ exports.fetchAllArticles = async (
   sort_by = "created_at",
   order = "DESC",
   topic
-) => {};
+) => {
+  try {
+    const getSlugs = await fetchSlugs();
+    const topics = getSlugs.map((e) => e.slug);
+    if (
+      (topic !== undefined) &
+      (topics.some((slug) => slug === topic) === false)
+    ) {
+      return Promise.reject({ status: 404, message: "Topic not Found" });
+    }
+    let query = `SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.created_at, articles.votes, COUNT(comments.article_id) AS comment_count FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id`;
+    if (topic === undefined) {
+      query += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`;
+    } else {
+      query += ` WHERE articles.topic = '${topic}' GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`;
+    }
+    const { rows } = await db.query(query);
+    return rows;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 exports.fetchArticleById = async (id) => {
   try {

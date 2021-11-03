20cffd1967c6787acc3562783c372bc494269251
BalliAsghar <ballihacker@outlook.com>
2021-11-03 15:09:40 +0000

    ✓ GET /api

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,9 @@ const apiRouter = require("express").Router();
 const topicsRouter = require("./topics.Router");
 const articlesRouter = require("./articles.Router");
 const commentsRouter = require("./comments.router");
+const endpoints = require("../endpoints.json");
 
-apiRouter.get("/", (req, res) => res.send("This Api Endpoint"));
+apiRouter.get("/", (req, res) => res.send(endpoints));
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);

1c5ed4c5e087bbfd219044931013fb5418dd7ad8
BalliAsghar <ballihacker@outlook.com>
2021-11-03 22:49:42 +0000

    Further Routes - GET /api/users/:username

--- models/users.model.js
+++ models/users.model.js
@@ -10,3 +10,17 @@ exports.fetchUsers = async () => {
     return Promise.reject(error);
   }
 };
+
+exports.fetchUser = async (username) => {
+  try {
+    const { rows } = await db.query(
+      "SELECT * FROM users WHERE username = $1;",
+      [username]
+    );
+    if (!rows.length)
+      return Promise.reject({ status: 404, message: "User Not Found" });
+    return rows[0];
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
--- routers/users.Router.js
+++ routers/users.Router.js
@@ -1,7 +1,9 @@
 const usersRouter = require("express").Router();
-const { getUsers } = require("../controllers/users.controller");
+const { getUsers, getUser } = require("../controllers/users.controller");
 
 // GET - /api/users - Get All Users
 usersRouter.get("/", getUsers);
+// GET - /api/users/:username - Get User Profile
+usersRouter.get("/:username", getUser);
 
 module.exports = usersRouter;

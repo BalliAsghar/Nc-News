3a51e7b7ebe1eed81393d5fe24c2139d1da20e32
BalliAsghar <ballihacker@outlook.com>
2021-11-05 10:02:14 +0000

    🧹

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,13 +1,13 @@
 module.exports = (err, req, res, next) => {
-  if (err?.code === "42P01") {
+  if (err.code === "42P01") {
     res.status(500).send({ message: "Internal Error" });
-  } else if (err?.code === "3D000") {
+  } else if (err.code === "3D000") {
     res.status(500).send({ message: "Database does not exist" });
-  } else if (err?.code === "22P02") {
+  } else if (err.code === "22P02") {
     res.status(400).send({ message: "Invalid Id" });
-  } else if (err?.code === "42703") {
+  } else if (err.code === "42703") {
     res.status(400).send({ message: "Bad Query" });
-  } else if (err?.code === "42601") {
+  } else if (err.code === "42601") {
     res.status(400).send({ message: "Bad Query" });
   } else {
     res.status(err.status).send({ message: err.message });

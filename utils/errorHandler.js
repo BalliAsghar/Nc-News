74ac32a8857b7102054b99e5009bed9fa7754cc2
BalliAsghar <ballihacker@outlook.com>
2021-11-05 12:00:23 +0000

    Error handler refactored

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,15 +1,21 @@
-module.exports = (err, req, res, next) => {
-  if (err.code === "42P01") {
-    res.status(500).send({ message: "Internal Error" });
-  } else if (err.code === "3D000") {
-    res.status(500).send({ message: "Database does not exist" });
-  } else if (err.code === "22P02") {
-    res.status(400).send({ message: "Invalid Id" });
-  } else if (err.code === "42703") {
-    res.status(400).send({ message: "Bad Query" });
-  } else if (err.code === "42601") {
-    res.status(400).send({ message: "Bad Query" });
-  } else {
-    res.status(err.status).send({ message: err.message });
+module.exports = (err, _req, res, _next) => {
+  switch (err.code) {
+    case "42P01":
+      res.status(500).send({ message: "Internal Error" });
+      break;
+    case "3D000":
+      res.status(500).send({ message: "Database does not exist" });
+      break;
+    case "22P02":
+      res.status(400).send({ message: "Invalid Id" });
+      break;
+    case "42703":
+      res.status(400).send({ message: "Bad Query" });
+      break;
+    case "42601":
+      res.status(400).send({ message: "Bad Query" });
+      break;
+    default:
+      res.status(err.status).send({ message: err.message });
   }
 };
